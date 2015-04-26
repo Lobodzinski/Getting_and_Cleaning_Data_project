48df72947f7a565fd4ca91f1a84a13de6fbcb103
Lobodzinski <bogdan.lobodzinski@gmx.net>
2015-04-27 01:12:39 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -78,4 +78,5 @@ Result<-aggregate(Data.x, Data.s, mean)
 
 # answer: Result
 # saving the Result data:
-write.csv(Result,file='submission.csv', quote=FALSE,row.names=FALSE)
+#write.csv(Result,file='submission.csv', quote=FALSE,row.names=FALSE)
+write.table(Result,file = "submission.txt",row.names=FALSE)
